a1fd257985bcd63f8056fffeaa7eca697c3ed087
Colin Coleman <colin.coleman@kontrollsmart.com>
2019-01-21 12:56:04 +0100

    add debug line to see why file not found by pipeline

--- .ci/tasks/inject-secrets/task.sh
+++ .ci/tasks/inject-secrets/task.sh
@@ -2,6 +2,7 @@
 export DIR="${PWD}"
 cp -a ${DIR}/source/. ${DIR}/secret-source/
 cd ${DIR}/secret-source/examples/${directory}
+pwd
 sed -i 's#<test-account-id>#'${AWS_ACCOUNT_ID}'#g' main.tf
 sed -i 's#<kms-key-id>#'${KMS_KEY_ID}'#g' main.tf
 sed -i 's#<route53-zone>#'${ROUTE53_ZONE}'#g' main.tf
