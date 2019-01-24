7680fbf5e7a5718e51b5275a106f51fe49aefa13
Colin Coleman <colin.coleman@kontrollsmart.com>
2019-01-24 16:58:43 +0100

    set SSM parameters for test

--- .ci/tasks/apply/task.sh
+++ .ci/tasks/apply/task.sh
@@ -2,7 +2,6 @@
 export DIR="${PWD}"
 cp -a ${DIR}/source/. ${DIR}/secret-source/
 cd ${DIR}/secret-source/examples/${directory}
-rm -rf .terraform
 terraform init
 terraform apply --auto-approve
 terraform output -json > ${DIR}/terraform-out/terraform-out.json
--- examples/default/init/main.tf
+++ examples/default/init/main.tf
@@ -19,6 +19,8 @@ provider "aws" {
 }
 
 locals {
+  name_prefix = "sonarqube-default-test"
+
   tags = {
     terraform   = "true"
     environment = "example"
@@ -27,9 +29,65 @@ locals {
 }
 
 module "sonarqube-init" {
-  name_prefix = "sonarqube"
+  name_prefix = "${local.name_prefix}"
   source      = "../../../modules/init"
   tags        = "${local.tags}"
+}
+
+# Don't do this in production - secrets set like this will leak to build logs
+
+resource "aws_ssm_parameter" "sonarqube-github-auth-enabled" {
+  name   = "/${local.name_prefix}/github-auth-enabled"
+  type   = "SecureString"
+  value  = "true"
+  key_id = "${module.sonarqube-init.parameters_key_arn}"
+}
+
+resource "aws_ssm_parameter" "sonarqube-github-client-id" {
+  name   = "/${local.name_prefix}/github-client-id"
+  type   = "SecureString"
+  value  = "<github-client-id>"
+  key_id = "${module.sonarqube-init.parameters_key_arn}"
+}
+
+resource "aws_ssm_parameter" "sonarqube-github-client-secret" {
+  name   = "/${local.name_prefix}/github-client-secret"
+  type   = "SecureString"
+  value  = "<github-client-secret>"
+  key_id = "${module.sonarqube-init.parameters_key_arn}"
+}
+
+resource "aws_ssm_parameter" "sonarqube-github-organizations" {
+  name   = "/${local.name_prefix}/github-organizations"
+  type   = "SecureString"
+  value  = "<github-organisations>"
+  key_id = "${module.sonarqube-init.parameters_key_arn}"
+}
+
+resource "random_string" "sonarqube-admin-username" {
+  length  = 10
+  special = false
+  number  = false
+}
+
+resource "aws_ssm_parameter" "sonarqube-admin-username" {
+  name   = "/${local.name_prefix}/admin-username"
+  type   = "SecureString"
+  value  = "${random_string.sonarqube-admin-username.result}"
+  key_id = "${module.sonarqube-init.parameters_key_arn}"
+}
+
+resource "random_string" "sonarqube-admin-password" {
+  length  = 10
+  special = false
+  number  = false
+}
+
+resource "aws_ssm_parameter" "sonarqube-admin-password" {
+  name   = "/${local.name_prefix}/admin-password"
+  type   = "SecureString"
+  value  = "${random_string.sonarqube-admin-password.result}"
+  key_id = "${module.sonarqube-init.parameters_key_arn}"
 }
 
 output "parameters_key_arn" {
