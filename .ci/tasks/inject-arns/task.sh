83f4662f10a6e4f31390856d22abfcd1defa15e6
Colin Coleman <colin.coleman@kontrollsmart.com>
2019-01-29 13:21:11 +0100

    add debug messages

--- .ci/tasks/inject-arns/task.sh
+++ .ci/tasks/inject-arns/task.sh
@@ -3,5 +3,7 @@ set -eu
 export DIR="${PWD}"
 cp -a ${DIR}/source/. ${DIR}/secret-source/
 PARAMETERS_KEY_ARN=`cat terraform-out-init/terraform-out.json | jq -r '.parameters_key_arn'`
+cat terraform-out-init/terraform-out.json
+echo $PARAMETERS_KEY_ARN
 cd ${DIR}/secret-source/examples/${directory}
 sed -i 's#<parameters-key-arn>#'${PARAMETERS_KEY_ARN}'#g' main.tf
