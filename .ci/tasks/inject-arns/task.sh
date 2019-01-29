333a76f3a22eba6f41dfcab8f2a36f6bd8f80867
Colin Coleman <colin.coleman@kontrollsmart.com>
2019-01-29 12:57:02 +0100

    change order so that command executes from correct location

--- .ci/tasks/inject-arns/task.sh
+++ .ci/tasks/inject-arns/task.sh
@@ -2,8 +2,6 @@
 set -eu
 export DIR="${PWD}"
 cp -a ${DIR}/source/. ${DIR}/secret-source/
-cd ${DIR}/secret-source/examples/${directory}
 PARAMETERS_KEY_ARN=`cat terraform-out-init/terraform-out.json | jq -r '.parameters_key_arn'`
-CERTIFICATE_ARN=`cat terraform-out-init/terraform-out.json | jq -r '.certificate_arn'`
+cd ${DIR}/secret-source/examples/${directory}
 sed -i 's#<parameters-key-arn>#'${PARAMETERS_KEY_ARN}'#g' main.tf
-sed -i 's#<certificate-arn>#'${CERTIFICATE_ARN}'#g' main.tf
