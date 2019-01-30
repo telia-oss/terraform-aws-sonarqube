e18008c80350fde03d5a152f00fc6612f116f413
Colin Coleman <colin.coleman@kontrollsmart.com>
2019-01-30 10:16:33 +0100

    add default region

--- .ci/tasks/clean-up/task.sh
+++ .ci/tasks/clean-up/task.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eu
 export DIR="${PWD}"
+export AWS_DEFAULT_REGION=eu-west-1
 cd ${DIR}/secret-source/examples/${directory}
 DB_INDENTIFIER=`grep name_prefix main.tf | sed 's/^[^"]*"\([^"]*\)".*/\1/'`-final-none
 aws rds delete-db-snapshot --db-snapshot-identifier $DB_INDENTIFIER
