20ea2c38ec160473287386116797096fce03621d
SadriG91 <sadri.gargouri@teliacompany.com>
2021-04-06 11:41:31 +0200

    removed line comment on examples/default/init/main.tf

--- examples/default/init/main.tf
+++ examples/default/init/main.tf
@@ -1,20 +1,20 @@
 terraform {
   required_version = ">= 0.14"
 
-  # backend "s3" {
-  #   key            = "terraform-modules/development/terraform-aws-sonarqube/default-init.tfstate"
-  #   bucket         = "<test-account-id>-terraform-state"
-  #   dynamodb_table = "<test-account-id>-terraform-state"
-  #   acl            = "bucket-owner-full-control"
-  #   encrypt        = "true"
-  #   kms_key_id     = "<kms-key-id>"
-  #   region         = "eu-west-1"
-  # }
+  backend "s3" {
+    key            = "terraform-modules/development/terraform-aws-sonarqube/default-init.tfstate"
+    bucket         = "<test-account-id>-terraform-state"
+    dynamodb_table = "<test-account-id>-terraform-state"
+    acl            = "bucket-owner-full-control"
+    encrypt        = "true"
+    kms_key_id     = "<kms-key-id>"
+    region         = "eu-west-1"
+  }
 }
 
 provider "aws" {
-  region = "us-east-1"
-  #allowed_account_ids = ["<test-account-id>"]
+  region = "eu-west-1"
+  allowed_account_ids = ["<test-account-id>"]
 }
 
 locals {
